47108efa1ad6f3e2373dc20ce1b3164d2cf18e30
kdurkin5 <kdurkin5@kent.edu>
2025-09-07 21:50:13 -0400

    Assignment 1 Supporting Documents

---
+++ Assignment1.R
@@ -0,0 +1,8 @@
+# Assignment 1
+# Kristen Durkin
+# Data Source: 
+# https://raw.githubusercontent.com/allisonhorst/palmerpenguins/main/inst/extdata/penguins.csv
+
+
+#Quantitative Points = Bill Length and Body Mass
+#Categorical Points = Species and Sex
